97b9cccf22b7edce1d14246007c28361dde25a16
NIPE-SYSTEMS <dev.nipe.systems@gmail.com>
2019-09-22 11:33:03 +0200

    Add ticking publisher

--- main.go
+++ main.go
@@ -4,11 +4,40 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
+
+func tickingPublisher(client mqtt.Client) func() {
+	ticker := time.NewTicker(time.Second)
+	done := make(chan struct{}, 1)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+	loop:
+		for {
+			select {
+			case t := <-ticker.C:
+				client.Publish("qlcplus/tick", 0, false, t.String())
+			case <-done:
+				break loop
+			}
+		}
+
+		wg.Done()
+	}()
+
+	return func() {
+		ticker.Stop()
+		close(done)
+		wg.Wait()
+	}
+}
 
 func main() {
 	c := make(chan os.Signal, 1)
@@ -25,9 +54,14 @@ func main() {
 	}
 	defer client.Disconnect(250)
 
-	if token := client.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+	if token := client.Subscribe("go-mqtt/sample", 0, func(client mqtt.Client, msg mqtt.Message) {
+		log.Print(string(msg.Payload()))
+	}); token.Wait() && token.Error() != nil {
 		log.Fatal(token.Error())
 	}
+
+	cancel := tickingPublisher(client)
+	defer cancel()
 
 	log.Printf("Got signal %v, exiting.", <-c)
 }
